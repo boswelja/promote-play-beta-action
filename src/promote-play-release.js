596581bbac20118e0a975f2d4eb43b0de67a6cd7
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-27 21:01:36 +1200

    feature: Break library down into separate functions

--- src/promote-play-release.js
+++ src/promote-play-release.js
@@ -25,42 +25,23 @@ export async function run() {
     }
 
     // Write service account JSON to file, then set the proper env variable so auth can find it.
-    debug('Saving service account JSON for temporary use');
-    writeFileSync(serviceAccountFile, rawServiceAccountJson, {
-      encoding: 'utf8'
-    });
-    exportVariable("GOOGLE_APPLICATION_CREDENTIALS", serviceAccountFile);
+    storeServiceAccountJson(rawServiceAccountJson);
+
+    // Get publisher
+    const publisher = google.androidpublisher('v3');
 
     // Authenticate
-    debug('Creating auth client');
-    const auth = new google.auth.GoogleAuth({
-      scopes: ['https://www.googleapis.com/auth/androidpublisher']
-    });
-    const authClient = await auth.getClient();
+    const authClient = getAuthClient();
 
     // Create app edit
     info('Creating a new edit');
-    const publisher = google.androidpublisher('v3');
     const appEdit = await publisher.edits.insert({
       packageName: packageName,
       auth: authClient
     });
 
     // Get the current beta track
-    info('Getting current beta info');
-    const betaTrack = await publisher.edits.tracks.get({
-      auth: authClient,
-      packageName: packageName,
-      editId: appEdit.data.id,
-      track: 'beta'
-    });
-    debug('Mapping beta releases to production releases with new data')
-    const betaReleases = betaTrack.data.releases;
-    const prodReleases = betaReleases.map((release) => {
-      release.inAppUpdatePriority = inAppUpdatePriority;
-      release.userFraction = userFraction;
-      return release;
-    });
+    const prodReleases = mapBetaToProduction(authClient, appEdit.data.id, packageName);
 
     // Patch beta track to production and apply userFraction and inAppUpdatePriority to the release
     info('Switching beta release to production');
@@ -97,4 +78,37 @@ export async function run() {
 
 export async function cleanup() {
   unlinkSync(serviceAccountFile);
+}
+
+async function getAuthClient() {
+  debug('Creating auth client');
+  const auth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/androidpublisher']
+  });
+  return await auth.getClient();
+}
+
+async function storeServiceAccountJson(rawJson) {
+  debug('Saving service account JSON for temporary use');
+  writeFileSync(serviceAccountFile, rawJson, {
+    encoding: 'utf8'
+  });
+  exportVariable("GOOGLE_APPLICATION_CREDENTIALS", serviceAccountFile);
+}
+
+async function mapBetaToProduction(authClient, editId, packageName) {
+  info('Getting current beta info');
+  const betaTrack = await publisher.edits.tracks.get({
+    auth: authClient,
+    packageName: packageName,
+    editId: editId,
+    track: 'beta'
+  });
+  debug('Mapping beta releases to production releases with new data')
+  const betaReleases = betaTrack.data.releases;
+  return betaReleases.map((release) => {
+    release.inAppUpdatePriority = inAppUpdatePriority;
+    release.userFraction = userFraction;
+    return release;
+  });
 }
